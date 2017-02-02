b2e36c9dde78b0cb35ddd5a5cc24850712c5811f
Artem Zinoviev <arzinoviev@gmail.com>
2017-02-02 15:31:40 +0300

    basix test case example

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,8 +6,12 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use App\Post;
+
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions; // rols back transactions after tests finished
+
     /**
      * A basic test example.
      *
@@ -15,6 +19,25 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        $first = factory(Post::class)->create();
+
+        $second = factory(Post::class)->create([
+            'created_at' => \Carbon\Carbon::now()->subMonth()
+        ]);
+
+        $posts = Post::archives();
+
+        $this->assertEquals([
+            [
+                'year' => (int) $first->created_at->format('Y'),
+                'month' => $first->created_at->format('F'),
+                'published' => 2
+            ],
+            [
+                'year' => (int) $second->created_at->format('Y'),
+                'month' => $second->created_at->format('F'),
+                'published' => 2
+            ]
+        ], $posts);
     }
 }
