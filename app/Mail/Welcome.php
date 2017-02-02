4c33f07690e5a982d58da03e629a4a42cee1ed86
Artem Zinoviev <arzinoviev@gmail.com>
2017-02-02 17:54:53 +0300

    send email on registration

---
+++ app/Mail/Welcome.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Welcome extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user; // in this class any public property available to view
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.welcome');
+    }
+}
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome email.</title>
+</head>
+<body>
+    <h1>Welcome to our project, {{ $user->name }}</h1>
+</body>
+</html>
